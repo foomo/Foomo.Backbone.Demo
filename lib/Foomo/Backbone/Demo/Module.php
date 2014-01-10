3025bd878271ab92704c79fb60541e8f54f8764b
Jan Halfar <jan@bestbytes.com>
2014-01-10 09:41:17 +0100

    added missing less module dependency

--- lib/Foomo/Backbone/Demo/Module.php
+++ lib/Foomo/Backbone/Demo/Module.php
@@ -65,6 +65,7 @@ class Module extends \Foomo\Modules\ModuleBase
 	{
 		return array(
 			\Foomo\Modules\Resource\Module::getResource('Foomo.Backbone', self::VERSION),
+			\Foomo\Modules\Resource\Module::getResource('Foomo.Less', '1.1.*'),
 			\Foomo\Modules\Resource\Module::getResource('Foomo.Jasmine', '0.1.*')
 			// get a run mode independent folder var/<runMode>/test
 			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'test'),
