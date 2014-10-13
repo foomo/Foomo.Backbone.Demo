f447bf6a85dac8bd856ce6aef3c924176c38cbb2
ulrich <info@baltzer-consulting.de>
2014-10-13 09:45:40 +0200

    fixed dependencies to SimpleData

--- lib/Foomo/Backbone/Demo/Module.php
+++ lib/Foomo/Backbone/Demo/Module.php
@@ -66,7 +66,9 @@ class Module extends \Foomo\Modules\ModuleBase
 		return array(
 			\Foomo\Modules\Resource\Module::getResource('Foomo.Backbone', self::VERSION),
 			\Foomo\Modules\Resource\Module::getResource('Foomo.Less', '1.1.*'),
-			\Foomo\Modules\Resource\Module::getResource('Foomo.Jasmine', '0.1.*')
+			\Foomo\Modules\Resource\Module::getResource('Foomo.Jasmine', '0.1.*'),
+			\Foomo\Modules\Resource\Module::getResource('Foomo.SimpleData', '0.3.*')
+
 			// get a run mode independent folder var/<runMode>/test
 			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'test'),
 			// and a file in it
